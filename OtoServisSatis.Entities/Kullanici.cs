9028fec70ea4d9eea21bd91330fd538181387042
atillakokcu <kokcu.ati@gmail.com>
2023-05-10 00:55:49 +0300

    Admin page kullanıcı ve roller oluşturulması

--- OtoServisSatis.Entities/Kullanici.cs
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -7,16 +7,17 @@ namespace OtoServisSatis.Entities
         public int Id { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Adı")]
         public string Adi { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50), Display(Name = "Soyadı")]
         public string Soyadi { get; set; }
 
         [StringLength(50)]
         public string Email { get; set; }
 
         [StringLength(20)]
-        public string Telefon { get; set; }
+        public string? Telefon { get; set; } // zorunlu olmaması için soruı işareti koyuyoruz
 
         [StringLength(50)]
         public string KullaniciAdi { get; set; }
@@ -24,6 +25,8 @@ namespace OtoServisSatis.Entities
         [StringLength(50)]
         public string Sifre  { get; set; }
         public bool AktifMi { get; set; }
+
+        [Display(Name="Eklenme tarihi"),ScaffoldColumn(false)] // html ekranda bu property için bir alan oluşturmaz
         public DateTime? EklemeTarihi { get; set; } = DateTime.Now; // ? işareti ile nullable yaptık ve otomatik olarak bu günün tarihi oalcak şekilde ayarlardık.
         public int  RolId { get; set; }
         public virtual Rol? Rol { get; set; }
---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")] // Areanın içinde olduğu için bunu yazmamız şart yoksa controller çalışmaz
+
+    public class UsersController : Controller
+    {
+        private readonly IService<Kullanici> _service;
+        private readonly IService<Rol> _serviceRol;
+
+        public UsersController(IService<Kullanici> service, IService<Rol> serviceRol) // Kurucu method da servis burada tanımladık
+        {
+            _service = service;
+            _serviceRol = serviceRol;
+        }
+
+
+        // GET: UsersController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsersController/Create
+        public async Task<ActionResult> CreateAsync()
+        {
+            ViewBag.RolId = new SelectList(await _serviceRol.GetAllAsync(),"Id","Adi"); // Html tarafında dropdownlist oluşturmak için buradaki veriyi selectlis olarak gönderdik ve Id ve Adı olacak şekilde oluşturduk
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Kullanici kullanici)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.AddAsync(kullanici);
+                    _service.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu");
+                    
+                }
+
+            }
+            return View(kullanici);
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
